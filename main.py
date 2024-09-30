0df5918010c1616e506247602a3cd33cdcbe12f9
r1cardohj <2413302357@qq.com>
2024-09-30 15:09:02 +0800

    fix: command init will set pwd to bip home path

--- main.py
+++ main.py
@@ -24,13 +24,16 @@ def cli():
 
 @cli.command()
 def init():
-
     if not BASE_DIR.exists():
         BASE_DIR.mkdir()
     if not PATCH_DIR.exists():
         PATCH_DIR.mkdir()
     if not CONF_FILE.exists():
+        pwd = Path('.').resolve()
+        BASE_CONFIGS['BIPHOME'] = str(pwd)
+        print(BASE_CONFIGS)
         CONF_FILE.write_text(json.dumps(BASE_CONFIGS))
+
 
 @cli.command()
 def configs():
